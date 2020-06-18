20f682af86b73768ee5f42da05362135cff6e846
juanjose <juanjosecg@ufps.edu.co>
2020-06-18 09:05:24 -0500

    Subiendo

--- src/main/java/com/ufps/edu/config/PasswordSecurityConfig.java
+++ src/main/java/com/ufps/edu/config/PasswordSecurityConfig.java
@@ -1,5 +1,14 @@
 package com.ufps.edu.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordSecurityConfig {
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/ufps/edu/controller/LoginController.java
+++ src/main/java/com/ufps/edu/controller/LoginController.java
@@ -1,5 +1,47 @@
 package com.ufps.edu.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ufps.edu.model.dao.IUsuarioDao;
+import com.ufps.edu.model.entity.Usuario;
+
+@Controller
 public class LoginController {
+	
+	@Autowired
+	private IUsuarioDao usuarioDao;
+	
+	@Autowired
+	private BCryptPasswordEncoder enconder;
+	
+	@PostMapping("/login")
+	public String login(@RequestParam String usuario, @RequestParam String clave,Model model) {
+		if(usuario.isEmpty()||usuario==null||clave.isEmpty()||clave==null) {
+			model.addAttribute("error", "Por Favor diligencie bien el formulario");
+			return "login";
+		}
+		Optional<Usuario> opt=usuarioDao.findByUsuario(usuario);
+		if(opt.isEmpty()) {
+			model.addAttribute("error", "Este usuario no existe");
+			return "login";
+		}
+		Usuario user=opt.get();
+		
+		if(!enconder.matches(clave, user.getClave())) {
+			model.addAttribute("error", "clave Incorrecta");
+			return "login";
+		}
+		return "indexOp2";
+
+		
+	}
 
 }
